b79c00a0b7998302e0294f9df109c67ddab15434
Adrien Kara <adrien@iglou.eu>
2023-12-13 18:39:27 +0100

    🐛 fix: wait the end of the animation
    Signed-off-by: Adrien Kara <adrien@iglou.eu>

--- src/credits/opening_credits.gd
+++ src/credits/opening_credits.gd
@@ -4,9 +4,9 @@ signal credits_finished
 signal credits_started
 
 func run():
-	$CreditsAnimation.stop(false)
 	await get_tree().create_timer(1.0).timeout
 	$CreditsAnimation.play("GlobalOpeningCreditsAnimation")
+	await credits_finished
 
 func _on_credits_animation_animation_finished(anim_name):
 	Debug.print("Animation finished: " + anim_name)

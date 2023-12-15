51c2e91cc10db6a95d79ccd589f10d33dfc12c06
gil-x <gino.ladowitch@gmail.com>
2023-12-15 13:48:46 +0100

    player assets

--- src/player/player.gd
+++ src/player/player.gd
@@ -5,20 +5,12 @@ extends CharacterBody2D
 @export var speed = 300.0
 @export var jump_velocity = -400.0
 
-#func test_while():
-	#print("start")
-	#var i = 0
-	#while i < 10:
-		#print(i)
-		#if i == 3:
-			#return
-		#i += 1
-		
+
 func _ready():
-	#test_while()
 	$Phosphorescence.load_rate = 0.007
 
 func _physics_process(delta):
+	$AnimatedSprite2D.animation = "idle"
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -26,6 +18,13 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * speed
+		$AnimatedSprite2D.animation = "run"
+		if velocity.x > 0:
+			
+			$AnimatedSprite2D.flip_h = false
+		else:
+			#$AnimatedSprite2D.animation = "run"
+			$AnimatedSprite2D.flip_h = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 	if move_and_slide():

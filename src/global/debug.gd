e036d145614a9ff4f739b3c1bbf92d123b3adeaf
Adrien Kara <adrien@iglou.eu>
2023-12-17 13:30:40 +0100

    ✨ feat: debug with trace
    Signed-off-by: Adrien Kara <adrien@iglou.eu>

--- src/global/debug.gd
+++ src/global/debug.gd
@@ -1,7 +1,20 @@
 extends Node
 
-func print(msg: String) -> void:
+func print(params: Array) -> void:
 	if not OS.is_debug_build():
 		return
 
-	print("[DEBUG] " + msg)
+	var stack = get_stack()
+	if stack.size() > 1:
+		stack = stack[1]
+	else:
+		stack = ["UNKNOWN", "0", "_"]
+
+	print(
+		"[DEBUG] (%s:%s %s) : %s" % [
+			stack["source"],
+			str(stack["line"]),
+			stack["function"],
+			params,
+		]
+	)
